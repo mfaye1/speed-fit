4a8a3ec704e746fa26db055385345f3829346370
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-02-25 00:38:00 -0500

    le header et footer et logo png

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mamadou Faye
- * Date: 22/02/2017
- * Time: 17:13
- */
+<footer>
+    <div>
+        <ul>
+            <li>Accueil</li>
+            <li>Activités</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-8">
+            <p></p>
+        </div>
+        <div class="row col-4">
+            <a href="www.google.com" class="col-4"> <img src="../images/Google+.PNG" alt="le logo google+ du footer"  ></a>
+            <a href="www.facebook.com" class="col-4"> <img src="../images/Logo-facebook-footer.png" alt="le logo facebook du footer" ></a>
+            <a href="www.twitter.com" class="col-4"> <img src="../images/twitter.png" alt="le logo twitter du footer"  ></a>
+        </div>
+    </div>
+</footer>
