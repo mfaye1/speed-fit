d0b0deba37516f7b0a726613e63ef714f55486c6
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-02-24 23:59:15 -0500

    le  header et l'essentiel du top

--- views/header.php
+++ views/header.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mamadou Faye
- * Date: 22/02/2017
- * Time: 17:13
- */
+<header>
+    <div id="menu_fixe" class="row">
+        <div class="col-6">
+            <img src="" alt="le logo du site">
+        </div>
+        <div class="col-6">
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Activités</li>
+                    <li>Contact</li>
+                    <li>Connexion</li>
+                    <li>Inscription</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div id="second_menu">
+        <nav>
+            <ul>
+                <li>NATATION</li>
+                <li>FITNESS</li>
+                <li>ARTS MARTIAUX</li>
+                <li>COURSE A PIED</li>
+                <li>VELOS</li>
+            </ul>
+        </nav>
+    </div>
+</header>
