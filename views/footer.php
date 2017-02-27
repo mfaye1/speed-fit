a83dce66aeed67756acf60fd3c1da11796f2d6b4
Willy Verrier <wverrier@isimtl.local>
2017-02-27 10:37:30 -0500

    Footer css

--- views/footer.php
+++ views/footer.php
@@ -1,20 +1,18 @@
 <footer>
-    <div>
+    <div id="bloc_menu_footer" class="row">
         <ul>
-            <li>Accueil</li>
-            <li>Activités</li>
-            <li>Contact</li>
+            <li class="col-4">Accueil</li>
+            <li class="col-4">Activités</li>
+            <li class="col-4">Contact</li>
         </ul>
     </div>
     <div class="row">
-        <div class="col-8">
-            <p></p>
-        </div>
-        <div class="row col-4">
+        <div id="logo_sociaux" class="row col-4">
             <a href="www.google.com" class="col-4"> <img src="images/Google+.PNG" alt="le logo google+ du footer"  ></a>
             <a href="www.facebook.com" class="col-4"> <img src="images/Logo-facebook-footer.png" alt="le logo facebook du footer" ></a>
             <a href="www.twitter.com" class="col-4"> <img src="images/twitter.png" alt="le logo twitter du footer"  ></a>
         </div>
+        <p class="col-12">Copyright</p>
     </div>
 </footer>
 </body>
