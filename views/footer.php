f37f4385f2f53266cab6bc5ecb1f65ea7e525c24
Willy Verrier <wverrier@isimtl.local>
2017-02-27 12:36:21 -0500

    petite modif footer

--- views/footer.php
+++ views/footer.php
@@ -8,7 +8,7 @@
     </div>
     <div class="row">
         <div id="logo_sociaux" class="row col-4">
-            <a href="www.google.com" class="col-4"> <img src="images/Google+.PNG" alt="le logo google+ du footer"  ></a>
+            <a href="www.google.com" class="col-4"> <img src="images/Google+.png" alt="le logo google+ du footer"  ></a>
             <a href="www.facebook.com" class="col-4"> <img src="images/Logo-facebook-footer.png" alt="le logo facebook du footer" ></a>
             <a href="www.twitter.com" class="col-4"> <img src="images/twitter.png" alt="le logo twitter du footer"  ></a>
         </div>
