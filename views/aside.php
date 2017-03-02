c94477f1ab3862e9b8555340d32db90cfd21dbb8
Willy Verrier <wverrier@isimtl.local>
2017-03-02 09:25:38 -0500

    Changement du css aside

--- views/aside.php
+++ views/aside.php
@@ -7,46 +7,17 @@
     </div>
     <div>
         <ul id="menu-deroulant">
-            <li><a href="#">Type de jeu</a>
+            <li><a href="#">Categories</a>
                 <ul>
-                    <li><a href="catalogue.php?catid=8">PS3</a></li>
-                    <li><a href="catalogue.php?catid=1">PS4</a></li>
-                    <li><a href="catalogue.php?catid=5">PS VITA / PSP</a></li>
-                    <li><a href="catalogue.php?catid=3">PC</a></li>
-                    <li><a href="catalogue.php?catid=7">Xbox 360</a></li>
-                    <li><a href="catalogue.php?catid=6">NINTENDO / DS</a></li>
+                    <li><a href="catalogue.php?catid=8">Natation</a></li>
+                    <li><a href="catalogue.php?catid=1">Fitness</a></li>
+                    <li><a href="catalogue.php?catid=5">Arts Martiaux</a></li>
+                    <li><a href="catalogue.php?catid=3">Course a pied</a></li>
+                    <li><a href="catalogue.php?catid=7">Velos</a></li>
+                    <li><a href="catalogue.php?catid=6">Centre de massage</a></li>
                 </ul>
             </li><!--
  -->
-            <li><a href="#">Categorie</a>
-                <ul>
-                    <li><a href="#">ARCADE</a></li>
-                    <li><a href="#">ACTION</a></li>
-                    <li><a href="#">AVENTURE</a></li>
-                    <li><a href="#">SPORT</a></li>
-                    <li><a href="#">COMBAT</a></li>
-                    <li><a href="#">COURSE</a></li>
-                </ul>
-            </li><!--
- -->
-            <li><a href="#">NOUVEAUTES</a>
-                <ul>
-                    <li><a href="#">DOUBLE DRAGON 4</a></li>
-                    <li><a href="#">SNIPER GHOST</a></li>
-                    <li><a href="#">RESIDENT EVIL 7</a></li>
-                    <li><a href="#">NFS </a></li>
-                    <li><a href="#">FIFA 17</a></li>
-                </ul>
-            </li><!--
- -->
-            <li><a href="#">POUR ENFANT</a>
-                <ul>
-                    <li><a href="#">SPIDER-MAN</a></li>
-                    <li><a href="#">SONIC</a></li>
-                    <li><a href="#">ANGRY-BIRD</a></li>
-                    <li><a href="#">ZELDA</a></li>
-                </ul>
-            </li>
         </ul>
     </div>
 </aside>
