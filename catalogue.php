a50a3a1c52732ce555ffb5159d13f5b1bf745b0e
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-03-02 09:02:49 -0500

    f

---
+++ catalogue.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mamadou Faye
+ * Date: 01/03/2017
+ * Time: 18:47
+ */
---
+++ views/aside.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mamadou Faye
+ * Date: 02/03/2017
+ * Time: 08:52
+ */
