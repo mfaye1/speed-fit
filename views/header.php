3290e3bdd1db20e0d16a4d8ed327cd13b1d67c1d
Mamadou Faye <mamadou.faye1@yahoo.fr>
2017-03-03 20:35:08 -0500

    menu

--- views/header.php
+++ views/header.php
@@ -64,13 +64,10 @@ if (is_logged_in() && array_key_exists('logout_btn', $_POST)) {
 <body>
 <header>
     <div id="menu_fixe" class="row">
-<<<<<<< HEAD
         <div class="col-6">
             <img src="images/logo.png" alt="le logo du site">
-        <div class="col-6 col-m-12">
-            <a href="index.php"> <img src="images/logo.png" alt="le logo du site"></a>
         </div>
-        <div class="col-6 col-m-12">
+        <div class="col-6">
             <nav>
                 <ul class="row">
                     <li class="col-2"><a href="index.php"> Accueil</a></li>
@@ -93,11 +90,6 @@ if (is_logged_in() && array_key_exists('logout_btn', $_POST)) {
                     <li class='col-2'><a href='inscription.php'>Inscription</a></li> ";
 
                  }?>
-                    <li class="col-2 col-m-12"><a href="index.php"> Accueil</a></li>
-                    <li class="col-2 col-m-12"><a href="catalogue.php"> Activités</a></li>
-                    <li class="col-2 col-m-12"><a href="contact.php"> Contact</a></li>
-                    <li class="col-2 col-m-12"><a href="#"> Connexion</a></li>
-                    <li class="col-2 col-m-12"><a href="inscription.php"> Inscription</a></li>
                 </ul>
             </nav>
         </div>
